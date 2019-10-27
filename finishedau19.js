dd6740489062b830ed72a508d24c6d811ff5e2de
Sang Lim <sanglim2@uw.edu>
2019-10-26 17:52:54 -0700

    partial hw

--- finishedau19.js
+++ finishedau19.js
@@ -2,47 +2,62 @@
 (function(){
     let data = ""
     let svgContainer = ""
+
     const measurements = {
         width: 500,
         height: 500,
         marginAll: 50
     }
 
-    window.onload = function() {
+    function test(time) {
         svgContainer = d3.select('body').append("svg")
             .attr('width', measurements.width)
             .attr('height', measurements.height);
-        d3.csv("Admission_Predict.csv")
+        d3.csv("dataEveryYear.csv")
             .then((csvData) => data = csvData)
-            .then(() => makeScatterPlot())
+            .then(() => makeScatterPlot(time))
     }
 
-    function makeScatterPlot() {
+    window.onload = function() {
+        test(this.document.getElementById("slide").value)
+        let time = document.getElementById("slide");
+        time.addEventListener('change',test(this.value));
+        }
+
+    function makeScatterPlot(time) {
         
-        let greScores = data.map((row) => parseInt(row["GRE Score"]))
-        let admitChances = data.map((row) =>  parseFloat(row["Admit"]))
-        
-        const limits = findMinMax(greScores, admitChances)
+        let fertilityRate = data.map((row) => parseInt(row["fertility_rate"]))
+        let lifeExpect = data.map((row) =>  parseFloat(row["life_expectancy"]))
+        let year = data.map((row) =>  parseFloat(row["time"]))
+
+        const limits = findMinMax(fertilityRate, lifeExpect,year)
 
         let scaleX = d3.scaleLinear()
-            .domain([limits.greMin - 5, limits.greMax])
+            .domain([limits.greMin, limits.greMax])
             .range([0 + measurements.marginAll, measurements.width - measurements.marginAll])
 
         let scaleY = d3.scaleLinear()
             .domain([limits.admitMax, limits.admitMin - 0.05])
             .range([0 + measurements.marginAll, measurements.height - measurements.marginAll])
+        
+          // Add a scale for bubble size
+        let scaleS = d3.scaleLinear()
+        .domain([limits.greMin, limits.greMax])
+        .range([ 1, 1.10]);
 
         drawAxes(scaleX, scaleY)
 
-        plotData(scaleX, scaleY)
+        plotData(scaleX, scaleY, scaleS, time)
     }
 
-    function findMinMax(greScores, admitChances) {
+    function findMinMax(fertilityRate, lifeExpect,year) {
         return {
-            greMin: d3.min(greScores),
-            greMax: d3.max(greScores),
-            admitMin: d3.min(admitChances),
-            admitMax: d3.max(admitChances)
+            greMin: d3.min(fertilityRate),
+            greMax: d3.max(fertilityRate),
+            admitMin: d3.min(lifeExpect),
+            admitMax: d3.max(lifeExpect),
+            yearMin: d3.min(year),
+            yearMax: d3.max(year)
         }
     }
 
@@ -63,17 +78,21 @@
             .call(yAxis)
     }
 
-    function plotData(scaleX, scaleY) {
-        const xMap = function(d) { return scaleX(+d["GRE Score"]) }
-        const yMap = function(d) { return scaleY(+d["Admit"]) }   
-        
+    function plotData(scaleX, scaleY, scaleS,time) {
+        const xMap = function(d) { return scaleX(+d["fertility_rate"]) }
+        const yMap = function(d) { return scaleY(+d["life_expectancy"]) }   
+        const sMap = function(d) { return scaleS(+d["pop_mlns"]) } 
+        //datas = d3.max(data, function(d) { return +d.time; })])
+        let filtered = data.filter(function(d) {
+            return d["time"] === time
+          })
         const circles = svgContainer.selectAll(".circle")
-            .data(data)
+            .data(filtered)
             .enter()
             .append('circle')
                 .attr('cx', xMap)
                 .attr('cy', yMap)
-                .attr('r', 3)
+                .attr('r', sMap)
                 .attr('fill', "#4286f4")
     }
 
